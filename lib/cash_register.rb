212f04159e79e9cdc3febcd18b6ade22a1811c38
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 13:04:01 +0300

    last_transaction

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,5 +23,9 @@ class CashRegister
         "There is no discount to apply."
       end
     end
-end
+  
+    def void_last_transaction
+      self.total = self.total - self.last_transaction
+    end
+  end
   
