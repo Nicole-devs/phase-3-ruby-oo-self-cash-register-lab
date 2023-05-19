604e06f90640d96869385cd0d3f9e8719ccbd3b0
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 13:01:34 +0300

    add_items

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,5 +4,14 @@ class CashRegister
     def initialize(discount = 0)
       @total = 0
       @discount = discount
+      @items = []
+    end
+  
+    def add_item(title, price, quantity = 1)
+      self.total += price * quantity
+      quantity.times do
+        items << title
+      end
     end
 end
+  
